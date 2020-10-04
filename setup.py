7c803f55b9094a87fb8f201507025ae30b50b25c
Zeta <fernando@flieber.com>
2020-10-04 20:36:54 -0300

    Bump version: 0.1.1 → 0.1.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ dependencies = [
 
 setup(
     name='aws-2fa',
-    version='0.1.1',
+    version='0.1.2',
     url='https://github.com/zetahernandez/aws-2fa',
     license='BSD',
     author='Zeta Hernandez',
